59619799271ee463a123479e61bd30dfe750857a
maikalal <pratim.skgc@gmail.com>
2013-12-16 11:00:12 +0000

    Update PaymentFilesProcessor.scala

--- src/main/scala/org/maikalal/ams/sim/payments/extractor/PaymentFilesProcessor.scala
+++ src/main/scala/org/maikalal/ams/sim/payments/extractor/PaymentFilesProcessor.scala
@@ -3,25 +3,24 @@ package org.maikalal.ams.sim.payments.extractor
 import java.io.File
 import net.liftweb.json.JsonAST.JObject
 import org.maikalal.ams.sim.payments.PaymentOrder
-import com.barclays.corp.ams.log.Logging
 import scala.util.Failure
 import scala.util.Try
 import scala.io.Codec
 import scala.util.Success
 import org.maikalal.ams.sim.utils.TransformationUtil
 import scala.io.Source
-
 import net.liftweb.json._
+import com.typesafe.scalalogging.slf4j.Logging
 
 object PaymentFilesProcessor extends Logging {
   def extractPaymentOrders(filePath: String)(implicit paymentFileCodec: Codec): List[PaymentOrder] = {
     //Check whether its a file or a folder, accordingly execute appropiate function
-    val paymentFiles = extractFilesFromFolder(new File(filePath))
+    val paymentFiles = TransformationUtil.extractFilesFromFolder(new File(filePath))
 
     val paymentOrders = paymentFiles.flatMap(extractPaymentOrderFromFile(_) match {
       case Success(poL) => poL
       case Failure(ex) =>
-        error(ex.toString())
+        logger.error(ex.toString())
         Nil
     })
     paymentOrders
@@ -43,16 +42,7 @@ object PaymentFilesProcessor extends Logging {
       Nil
   }
 
-  def extractFilesFromFolder(file: File): List[File] = {
-    def recur(file: File, tree: List[File]): List[File] = if (file.isFile()) List(file) ::: tree
-    else {
-      val subFiles = file.listFiles().filter(_.isFile()).toList ::: tree
-      val subDirectories = file.listFiles().filter(_.isDirectory()).toList
-      if (subDirectories.isEmpty) subFiles
-      else subDirectories.foldLeft(subFiles)((t, f) => recur(f, t) ::: t)
-    }
-    recur(file, List())
-  }
+
 
   def isEdifactPaymulPaymentData(data: List[String]): Boolean = data match {
     case head :: tail => head.contains("PAYMUL")
