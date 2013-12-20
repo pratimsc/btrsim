fe77676b569f39a59c304bd4c457ad0c9fae3130
maikalal <pratim.skgc@gmail.com>
2013-12-20 17:41:07 +0000

    Update PaymentJsonProcessor.scala

--- src/main/scala/org/maikalal/ams/sim/payments/extractor/PaymentJsonProcessor.scala
+++ src/main/scala/org/maikalal/ams/sim/payments/extractor/PaymentJsonProcessor.scala
@@ -7,8 +7,11 @@ import org.maikalal.ams.sim.payments.PaymentProcessor
 import net.liftweb.json.Formats
 import net.liftweb.json.parse
 import org.maikalal.ams.sim.payments.PaymentOrder
+import scala.util.Success
+import scala.util.Failure
+import com.typesafe.scalalogging.slf4j.Logging
 
-object PaymentJsonProcessor {
+object PaymentJsonProcessor extends Logging {
 
   def extractIncomingPaymentTransactionsFromPaymentOrdersJSON(paymentOrdersJson: String)(implicit formats: Formats): Try[List[AccountTransaction]] = Try {
     val poL = (parse(paymentOrdersJson) \\ "paymentOrders").extract[List[PaymentOrder]]
@@ -35,10 +38,27 @@ object PaymentJsonProcessor {
     List(parse(paymentOrdersJson).extract[PaymentOrder])
   }
 
-  def extractPaymentOrderListFromJson(paymentJson: String)(implicit formats: Formats): Try[List[PaymentOrder]] = Try {
-    extractsPaymentOrderListFromJSONPayment(paymentJson).
-      getOrElse(extractsPaymentOrderListFromJSONPaymentOrderArray(paymentJson).
-        getOrElse(extractsPaymentOrderListFromJSONPaymentOrder(paymentJson).getOrElse(Nil)))
+  def extractPaymentOrderListFromJson(paymentJson: String)(implicit formats: Formats): Try[List[PaymentOrder]] = Try {    
+    extractsPaymentOrderListFromJSONPayment(paymentJson)match {
+      case Success(poL) => poL
+      case Failure(ex) =>
+        logger.debug(s"Failed JSON parsing with error ${ex.toString()}")
+        logger.debug(s"Could not process the file on 1st attempt. Trying a differnt way.")
+        extractsPaymentOrderListFromJSONPaymentOrderArray(paymentJson) match {
+          case Success(poL) => poL
+          case Failure(ex) =>
+            logger.debug(s"Failed JSON parsing with error ${ex.toString()}")
+            logger.debug(s"Could not process the file on 2nd attempt. Trying a differnt way.")
+            extractsPaymentOrderListFromJSONPaymentOrder(paymentJson)match {
+              case Success(poL) => poL
+              case Failure(ex) =>
+                logger.debug(s"Failed JSON parsing with error ${ex.toString()}")
+                logger.debug(s"Could not process the file on 3rd attempt. ")
+                Nil
+            }
+        }
+    }
+
   }
 
   def extractPaymentOrderListFromJson(paymentJson: List[String])(implicit formats: Formats): Try[List[PaymentOrder]] = extractPaymentOrderListFromJson(paymentJson.mkString)
