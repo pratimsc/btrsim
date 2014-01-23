872f818b64ea3af2a2cb67a04deed4fef181fb92
maikalal <pratim.skgc@gmail.com>
2014-01-23 16:34:28 +0000

    Update PaymentProcessor.scala

--- src/main/scala/org/maikalal/ams/sim/payments/PaymentProcessor.scala
+++ src/main/scala/org/maikalal/ams/sim/payments/PaymentProcessor.scala
@@ -15,82 +15,70 @@ object PaymentProcessor {
     transactionPairList.flatMap(x => x)
   }
 
-  def generateTransactionPairs(pi: PaymentInstruction, originatorAccountNumber: UKAccountNumber) = {
+  def generateTransactionPairs(pi: PaymentInstruction, originatorAccountNumber: UKAccountNumber): List[AccountTransaction] = {
     //Initialize default/applicable values
-    val transactionValue = new Money(pi.monetaryAmount.amountInMinorCurrency.abs, pi.monetaryAmount.currencyCode)
-    val transactionDate = pi.paymentDate
     val transactionReferenceNumber = "0000"
 
     //Create pairs of Credit and Debit transaction from each payment instruction
     val transactions = if (pi.monetaryAmount.amountInMinorCurrency < 0L) {
-      //Debit the Beneficiary Account. Payment is DEBIT type.
-      val originatorCRTransaction = new AccountTransaction(
-        accountNumber = originatorAccountNumber,
-        originatingAccountNumber = pi.beneficiaryAccountNumber,
-        transactionValue = (-pi.monetaryAmount),
-        transacionDate = transactionDate,
-        //DirectFile - Credit
-        transactionCode = "85", tlaCode = "8",
-        transactionReferenceNumber = transactionReferenceNumber,
-        narrative1 = transactionValue.currencyCode match {
-          case CurrencyUnit.GBP => TransformationUtil.EMPTY_VALUE_STRING
-          case _ => "NTRF" + pi.beneficiaryReferenceNumberCR.getOrElse("NOBENREFCR")
-        },
-        narrative2 = transactionValue.currencyCode match {
-          case CurrencyUnit.GBP => pi.beneficiaryReferenceNumberCR.getOrElse(TransformationUtil.EMPTY_VALUE_STRING)
-          case _ => ""
-        })
-
-      //Create a transaction for the Payment beneficiary account
-      val beneficiaryDRTransaction = new AccountTransaction(
-        accountNumber = pi.beneficiaryAccountNumber,
-        originatingAccountNumber = originatorAccountNumber,
-        transactionValue = pi.monetaryAmount,
-        transacionDate = transactionDate,
-        //Transfer/CHAPS/Foreign - Debit
-        transactionCode = "82", tlaCode = "0",
-        transactionReferenceNumber = transactionReferenceNumber,
-        narrative1 = transactionValue.currencyCode match {
-          case CurrencyUnit.GBP => "EDI BGM " + pi.beneficiaryReferenceNumberCR.getOrElse("NOBENREFCR")
-          case _ => "NTRF" + pi.beneficiaryReferenceNumberCR.getOrElse("NOBENREFCR")
-        },
-        narrative2 = TransformationUtil.EMPTY_VALUE_STRING)
-      List(originatorCRTransaction, beneficiaryDRTransaction)
+      //Reverse the flow of money from Beneficiary to Originator account
+      generateTransactionPairs(
+        pi.copy(beneficiaryAccountNumber = originatorAccountNumber, monetaryAmount = -pi.monetaryAmount),
+        pi.beneficiaryAccountNumber)
     } else {
       //Credit the Beneficiary Account. Payment is CREDIT type.
       val originatorDRTransaction = new AccountTransaction(
         accountNumber = originatorAccountNumber,
         originatingAccountNumber = pi.beneficiaryAccountNumber,
         transactionValue = (-pi.monetaryAmount),
-        transacionDate = transactionDate,
+        transacionDate = pi.paymentDate,
         //DirectFile Debit - Debit
-        transactionCode = "82", tlaCode = "0",
+        transactionCode = "82",
+        tlaCode = generateTlaCodeForDebit(pi.paymentMode.getOrElse(0)),
         transactionReferenceNumber = transactionReferenceNumber,
-        narrative1 = transactionValue.currencyCode match {
-          case CurrencyUnit.GBP => "EDI BGM " + pi.beneficiaryReferenceNumberCR.getOrElse("NOBENREFCR")
-          case _ => "NTRF" + pi.beneficiaryReferenceNumberCR.getOrElse("NOBENREFCR")
-        },
-        narrative2 = TransformationUtil.EMPTY_VALUE_STRING)
+        narrative = generateNarrativeForDebit(pi.beneficiaryReferenceNumberCR.getOrElse("NOBENREFCR"), pi.paymentMode))
 
       //Create a transaction for the Payment beneficiary account
       val beneficiaryCRTransaction = new AccountTransaction(
         accountNumber = pi.beneficiaryAccountNumber,
         originatingAccountNumber = originatorAccountNumber,
         transactionValue = pi.monetaryAmount,
-        transacionDate = transactionDate,
+        transacionDate = pi.paymentDate,
         //Transfer/CHAPS/Foreign - Credit
-        transactionCode = "85", tlaCode = "8",
+        transactionCode = "85",
+        tlaCode = generateTlaCodeForCredit(pi.paymentMode.getOrElse(0)),
         transactionReferenceNumber = transactionReferenceNumber,
-        narrative1 = transactionValue.currencyCode match {
-          case CurrencyUnit.GBP => TransformationUtil.EMPTY_VALUE_STRING
-          case _ => "NTRF" + pi.beneficiaryReferenceNumberCR.getOrElse("NOBENREFCR")
-        },
-        narrative2 = transactionValue.currencyCode match {
-          case CurrencyUnit.GBP => pi.beneficiaryReferenceNumberCR.getOrElse("NOBENREFCR")
-          case _ => TransformationUtil.EMPTY_VALUE_STRING
-        })
+        narrative = generateNarrativeForCredit(pi.beneficiaryReferenceNumberCR.getOrElse("NOBENREFCR"), pi.paymentMode))
       List(beneficiaryCRTransaction, originatorDRTransaction)
     }
     transactions
+  }
+
+  def generateTlaCodeForCredit(paymentMode: Int) = paymentMode match {
+    case TransformationUtil.PaymentMode.IAT => "2"
+    case TransformationUtil.PaymentMode.BACS => "8"
+    case TransformationUtil.PaymentMode.CHAPS => "8"
+    case TransformationUtil.PaymentMode.FASTER_PAYMENT => "6"
+    case TransformationUtil.PaymentMode.CROSS_BORDER => "8"
+    case _ => "0"
+  }
+
+  def generateTlaCodeForDebit(paymentMode: Int) = paymentMode match {
+    case TransformationUtil.PaymentMode.IAT => "0"
+    case TransformationUtil.PaymentMode.BACS => "8"
+    case TransformationUtil.PaymentMode.CHAPS => "8"
+    case TransformationUtil.PaymentMode.FASTER_PAYMENT => "6"
+    case TransformationUtil.PaymentMode.CROSS_BORDER => "8"
+    case _ => "0"
+  }
+
+  def generateNarrativeForCredit(nar: String, paymentMode: Option[Int]) = paymentMode match {
+    case Some(TransformationUtil.PaymentMode.IAT) => "FTRF" + nar.trim + "*"
+    case _ => nar.trim()
+  }
+
+  def generateNarrativeForDebit(nar: String, paymentMode: Option[Int]) = paymentMode match {
+    case Some(TransformationUtil.PaymentMode.IAT) => "FTRF" + nar.trim
+    case _ => nar.trim()
   }
 }
