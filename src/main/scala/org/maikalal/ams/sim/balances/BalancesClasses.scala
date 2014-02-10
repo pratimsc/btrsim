889aafe7562ccd32fd5ead20c32594d91cb47432
maikalal <pratim.skgc@gmail.com>
2014-02-10 09:56:18 +0000

    Update BalancesClasses.scala

--- src/main/scala/org/maikalal/ams/sim/balances/BalancesClasses.scala
+++ src/main/scala/org/maikalal/ams/sim/balances/BalancesClasses.scala
@@ -1,64 +1,28 @@
 package org.maikalal.ams.sim.balances
 
-import scala.annotation.migration
-import scala.collection.immutable.HashMap
-import scala.math.BigDecimal.int2bigDecimal
-
 import org.maikalal.ams.sim.payments.AccountNumber
-import org.maikalal.ams.sim.payments.AccountTransaction
+import org.joda.time.DateTime
 import org.maikalal.ams.sim.payments.Money
-import org.maikalal.ams.sim.balances.AccountBalance._
+import org.maikalal.ams.sim.payments.AccountTransaction
 
-object BalanceProcessor {  
-  /*
-   * Takes in a reference Account Ledger. Will extract the information for all the transactions associated with this account
-   * from List of account transactions and create a new list of Account Ledgers 
-   */
-  def extractTransactionsRegisteredWithAccount(acc: AccountNumber, transactions: List[AccountTransaction]): Option[List[AccountLedger]] =
-    transactions.filter(tr => tr.accountNumber == acc) match {
-      case h :: t => Some(generateAccountBalancePerDate(h :: t).filter(al => al.account == acc))
-      case Nil => None
-    }
-
-  /*
-   * Loop through list of Account Ledger records and calculate the EOD Balance for the set of accounts. 
-   * Return a list where every Account Ledger is updated to include EOD balance.
-   */
-  def calculateProgressiveLedgerBalance(accLedgers: List[AccountLedger]): List[AccountLedger] = {
-    val sortedAccLedgers = accLedgers.sortWith((x, y) => x.ledgerDate.getMillis() < y.ledgerDate.getMillis())
-    val accList = accLedgers.groupBy(e => e.account).keys.toList
-
-    def ledgerBalanceForAnAccount(accLL: List[AccountLedger]): List[AccountLedger] = {
-      val sortedAccLL = accLL.sortWith((x, y) => x.ledgerDate.getMillis() < y.ledgerDate.getMillis())
-      val headAccLedger = sortedAccLL.head
-      val defaultCurrency = headAccLedger.balances.get(BALANCE_TYPE_DAILY).get.balance.currencyCode
-      val seedBalance = headAccLedger.balances.getOrElse(BALANCE_TYPE_PREV_EOD, new AccountBalance(BALANCE_TYPE_PREV_EOD, new Money(0, defaultCurrency)))
-      val (lastBalance, updatedList) = sortedAccLL.foldLeft((seedBalance, List[AccountLedger]())) { (acc, presentLedger) =>
-        val eodBal = new AccountBalance(BALANCE_TYPE_PREV_EOD, acc._1.balance + presentLedger.balances.get(BALANCE_TYPE_DAILY).get.balance)
-        val updLedger = presentLedger.balances + (BALANCE_TYPE_EOD -> eodBal)
-        (eodBal, new AccountLedger(presentLedger.account, presentLedger.ledgerDate, updLedger, presentLedger.transactions) :: acc._2)
-      }
-      updatedList.reverse
-    }
-
-    val processedAccLedgers = for (acc <- accList) yield ledgerBalanceForAnAccount(accLedgers.filter(x => x.account == acc))
-    processedAccLedgers.flatten
+case class AccountBalance(val `type`: String, val balance: Money) {
+  def +(that: AccountBalance) = that.`type` match {
+    case `type` => new AccountBalance(`type`, this.balance + that.balance)
+    case _ => this
   }
-
-  /*
-   * Create Daily Account Ledgers from the transactions provided  
-   */
-  def generateAccountBalancePerDate(transactions: List[AccountTransaction]): List[AccountLedger] = {
-    val gr = transactions.groupBy(tr => (tr.accountNumber, tr.transacionDate))
-    val ledgers = for {
-      ((accountNumber, transacionDate), trList) <- gr
-    } yield new AccountLedger(account = accountNumber,
-      ledgerDate = transacionDate,
-      balances = HashMap(BALANCE_TYPE_DAILY -> new AccountBalance(BALANCE_TYPE_DAILY,
-        trList.foldLeft(new Money(0, trList.head.transactionValue.currencyCode)) { (bal, tr) =>
-          bal + tr.transactionValue
-        })),
-      transactions = trList)
-    ledgers.toList
+  def -(that: AccountBalance) = that.`type` match {
+    case `type` => new AccountBalance(`type`, this.balance - that.balance)
+    case _ => this
   }
 }
+
+object AccountBalance {
+  val BALANCE_TYPE_DAILY = "BALANCE_TYPE_DAILY"
+  val BALANCE_TYPE_EOD = "BALANCE_TYPE_EOD"
+  val BALANCE_TYPE_PREV_EOD = "BALANCE_TYPE_PREV_EOD"
+}
+
+case class AccountLedger(val account: AccountNumber,
+  val ledgerDate: DateTime,
+  val balances: Map[String, AccountBalance],
+  val transactions: List[AccountTransaction])
