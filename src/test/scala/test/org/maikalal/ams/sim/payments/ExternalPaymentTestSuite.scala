6b2d9c1129cdbcda4881395fbc82c5d428763d29
maikalal <pratim.skgc@gmail.com>
2014-02-10 10:28:38 +0000

    Update ExternalPaymentTestSuite.scala

--- src/test/scala/test/org/maikalal/ams/sim/payments/ExternalPaymentTestSuite.scala
+++ src/test/scala/test/org/maikalal/ams/sim/payments/ExternalPaymentTestSuite.scala
@@ -15,6 +15,8 @@ import net.liftweb.json.NoTypeHints
 import net.liftweb.json.Serialization._
 import org.maikalal.ams.sim.payments.Money
 import org.joda.money.CurrencyUnit
+import scala.util.Success
+import scala.util.Failure
 
 @RunWith(classOf[JUnitRunner])
 class ExternalPaymentCreatorTestSuite extends FunSuite {
@@ -25,13 +27,15 @@ class ExternalPaymentCreatorTestSuite extends FunSuite {
     originatorReferenceNumberAEK = Some("Originator reference number"),
     beneficiaryReferenceNumberCR = Some("Beneficiary reference number"),
     monetaryAmount = new Money(9876543210L, CurrencyUnit.GBP),
-    paymentDate = new DateTime(2013, 11, 26, 0, 0))
+    paymentDate = new DateTime(2013, 11, 26, 0, 0),
+    paymentMode = Some(TransformationUtil.PaymentMode.CHAPS))
   val pi2 = PaymentInstruction(
     beneficiaryAccountNumber = new UKAccountNumber("112244", "44556688"),
     originatorReferenceNumberAEK = Some("Originator reference number"),
     beneficiaryReferenceNumberCR = Some("Beneficiary reference number"),
     monetaryAmount = new Money(9876543220L, CurrencyUnit.GBP),
-    paymentDate = new DateTime(2013, 11, 26, 0, 0))
+    paymentDate = new DateTime(2013, 11, 26, 0, 0),
+    paymentMode = Some(TransformationUtil.PaymentMode.CHAPS))
 
   //Test payment orders
   val po1 = new PaymentOrder(originatorAccountNumber = new UKAccountNumber("223344", "55667788"),
@@ -61,7 +65,7 @@ class ExternalPaymentCreatorTestSuite extends FunSuite {
   }
 
   test("TEST2 - Check whether it Able to parse Payment Instruction") {
-    val jsonStr = """{"beneficiaryAccountNumber":{"sortCode":"112233","accountNumber":"44556677"},"originatorReferenceNumberAEK":"Originator reference number","beneficiaryReferenceNumberCR":"Beneficiary reference number","monetaryAmount":{"amountInMinorCurrency":9876543210,"currencyCode":"GBP"},"paymentDate":"20131126"}"""
+    val jsonStr = """{"beneficiaryAccountNumber":{"sortCode":"112233","accountNumber":"44556677"},"originatorReferenceNumberAEK":"Originator reference number","beneficiaryReferenceNumberCR":"Beneficiary reference number","monetaryAmount":{"amountInMinorCurrency":9876543210,"currencyCode":"GBP"},"paymentDate":"20131126","paymentMode":3}"""
     //implicit val formats = net.liftweb.json.DefaultFormats ++ net.liftweb.json.ext.JodaTimeSerializers.all
     implicit val formats = net.liftweb.json.DefaultFormats ++
       List(new TransformationUtil.MyDateTimeSerializer(TransformationUtil.DT_FORMAT_CCYYMMDD),
@@ -73,7 +77,7 @@ class ExternalPaymentCreatorTestSuite extends FunSuite {
   }
 
   test("TEST3 - Check whether it is able to parse Payment Order") {
-    val jsonStr = """{"originatorAccountNumber":{"sortCode":"223344","accountNumber":"55667788"},"originatorReferenceNumberAEK":"Orginator Reference Number AEK","paymentInstructions":[{"beneficiaryAccountNumber":{"sortCode":"112233","accountNumber":"44556677"},"originatorReferenceNumberAEK":"Originator reference number","beneficiaryReferenceNumberCR":"Beneficiary reference number","monetaryAmount":{"amountInMinorCurrency":9876543210,"currencyCode":"GBP"},"paymentDate":"2013-11-26T00:00:00.000Z"}]}"""
+    val jsonStr = """{"originatorAccountNumber":{"sortCode":"223344","accountNumber":"55667788"},"originatorReferenceNumberAEK":"Orginator Reference Number AEK","paymentInstructions":[{"beneficiaryAccountNumber":{"sortCode":"112233","accountNumber":"44556677"},"originatorReferenceNumberAEK":"Originator reference number","beneficiaryReferenceNumberCR":"Beneficiary reference number","monetaryAmount":{"amountInMinorCurrency":9876543210,"currencyCode":"GBP"},"paymentDate":"2013-11-26T00:00:00.000Z","paymentMode":3}]}"""
     implicit val formats = net.liftweb.json.DefaultFormats ++
       List(new TransformationUtil.MyDateTimeSerializer("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"),
         new TransformationUtil.MyJodaCurrencyUnitSerializer,
@@ -83,7 +87,7 @@ class ExternalPaymentCreatorTestSuite extends FunSuite {
   }
 
   test("TEST4 - Check whether it create pairs of Transaction or Not from JSON string with Payment Order Array") {
-    val jsonStr = """[{"originatorAccountNumber":{"sortCode":"223344","accountNumber":"55667788"},"originatorReferenceNumberAEK":"Orginator Reference Number AEK","paymentInstructions":[{"beneficiaryAccountNumber":{"sortCode":"112233","accountNumber":"44556677"},"originatorReferenceNumberAEK":"Originator reference number","beneficiaryReferenceNumberCR":"Beneficiary reference number","monetaryAmount":{"amountInMinorCurrency":9876543210,"currencyCode":"GBP"},"paymentDate":"2013-11-26T00:00:00.000Z"}]},{"originatorAccountNumber":{"sortCode":"223345","accountNumber":"55667799"},"originatorReferenceNumberAEK":"Orginator Reference Number AEK","paymentInstructions":[{"beneficiaryAccountNumber":{"sortCode":"112233","accountNumber":"44556677"},"originatorReferenceNumberAEK":"Originator reference number","beneficiaryReferenceNumberCR":"Beneficiary reference number","monetaryAmount":{"amountInMinorCurrency":9876543210,"currencyCode":"GBP"},"paymentDate":"2013-11-26T00:00:00.000Z"},{"beneficiaryAccountNumber":{"sortCode":"112244","accountNumber":"44556688"},"originatorReferenceNumberAEK":"Originator reference number","beneficiaryReferenceNumberCR":"Beneficiary reference number","monetaryAmount":{"amountInMinorCurrency":9876543220,"currencyCode":"GBP"},"paymentDate":"2013-11-26T00:00:00.000Z"}]}]"""
+    val jsonStr = """[{"originatorAccountNumber":{"sortCode":"223344","accountNumber":"55667788"},"originatorReferenceNumberAEK":"Orginator Reference Number AEK","paymentInstructions":[{"beneficiaryAccountNumber":{"sortCode":"112233","accountNumber":"44556677"},"originatorReferenceNumberAEK":"Originator reference number","beneficiaryReferenceNumberCR":"Beneficiary reference number","monetaryAmount":{"amountInMinorCurrency":9876543210,"currencyCode":"GBP"},"paymentDate":"2013-11-26T00:00:00.000Z",,"paymentMode":3}]},{"originatorAccountNumber":{"sortCode":"223345","accountNumber":"55667799"},"originatorReferenceNumberAEK":"Orginator Reference Number AEK","paymentInstructions":[{"beneficiaryAccountNumber":{"sortCode":"112233","accountNumber":"44556677"},"originatorReferenceNumberAEK":"Originator reference number","beneficiaryReferenceNumberCR":"Beneficiary reference number","monetaryAmount":{"amountInMinorCurrency":9876543210,"currencyCode":"GBP"},"paymentDate":"2013-11-26T00:00:00.000Z"},{"beneficiaryAccountNumber":{"sortCode":"112244","accountNumber":"44556688"},"originatorReferenceNumberAEK":"Originator reference number","beneficiaryReferenceNumberCR":"Beneficiary reference number","monetaryAmount":{"amountInMinorCurrency":9876543220,"currencyCode":"GBP"},"paymentDate":"2013-11-26T00:00:00.000Z",,"paymentMode":3}]}]"""
     implicit val formats = net.liftweb.json.DefaultFormats ++
       List(new TransformationUtil.MyDateTimeSerializer("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"),
         new TransformationUtil.MyJodaCurrencyUnitSerializer,
@@ -93,7 +97,7 @@ class ExternalPaymentCreatorTestSuite extends FunSuite {
   }
 
   test("TEST5 - Check whether it create pairs of Transaction or Not from JSON string with Payment Orders") {
-    val jsonStr = """{"paymentOrders":[{"originatorAccountNumber":{"sortCode":"223344","accountNumber":"55667788"},"originatorReferenceNumberAEK":"Orginator Reference Number AEK","paymentInstructions":[{"beneficiaryAccountNumber":{"sortCode":"112233","accountNumber":"44556677"},"originatorReferenceNumberAEK":"Originator reference number","beneficiaryReferenceNumberCR":"Beneficiary reference number","monetaryAmount":{"amountInMinorCurrency":9876543210,"currencyCode":"GBP"},"paymentDate":"2013-11-26T00:00:00.000Z"}]},{"originatorAccountNumber":{"sortCode":"223345","accountNumber":"55667799"},"originatorReferenceNumberAEK":"Orginator Reference Number AEK","paymentInstructions":[{"beneficiaryAccountNumber":{"sortCode":"112233","accountNumber":"44556677"},"originatorReferenceNumberAEK":"Originator reference number","beneficiaryReferenceNumberCR":"Beneficiary reference number","monetaryAmount":{"amountInMinorCurrency":9876543210,"currencyCode":"GBP"},"paymentDate":"2013-11-26T00:00:00.000Z"},{"beneficiaryAccountNumber":{"sortCode":"112244","accountNumber":"44556688"},"originatorReferenceNumberAEK":"Originator reference number","beneficiaryReferenceNumberCR":"Beneficiary reference number","monetaryAmount":{"amountInMinorCurrency":9876543220,"currencyCode":"GBP"},"paymentDate":"2013-11-26T00:00:00.000Z"}]}]}"""
+    val jsonStr = """{"paymentOrders":[{"originatorAccountNumber":{"sortCode":"223344","accountNumber":"55667788"},"originatorReferenceNumberAEK":"Orginator Reference Number AEK","paymentInstructions":[{"beneficiaryAccountNumber":{"sortCode":"112233","accountNumber":"44556677"},"originatorReferenceNumberAEK":"Originator reference number","beneficiaryReferenceNumberCR":"Beneficiary reference number","monetaryAmount":{"amountInMinorCurrency":9876543210,"currencyCode":"GBP"},"paymentDate":"2013-11-26T00:00:00.000Z","paymentMode":3}]},{"originatorAccountNumber":{"sortCode":"223345","accountNumber":"55667799"},"originatorReferenceNumberAEK":"Orginator Reference Number AEK","paymentInstructions":[{"beneficiaryAccountNumber":{"sortCode":"112233","accountNumber":"44556677"},"originatorReferenceNumberAEK":"Originator reference number","beneficiaryReferenceNumberCR":"Beneficiary reference number","monetaryAmount":{"amountInMinorCurrency":9876543210,"currencyCode":"GBP"},"paymentDate":"2013-11-26T00:00:00.000Z","paymentMode":3},{"beneficiaryAccountNumber":{"sortCode":"112244","accountNumber":"44556688"},"originatorReferenceNumberAEK":"Originator reference number","beneficiaryReferenceNumberCR":"Beneficiary reference number","monetaryAmount":{"amountInMinorCurrency":9876543220,"currencyCode":"GBP"},"paymentDate":"2013-11-26T00:00:00.000Z","paymentMode":3}]}]}"""
     implicit val formats = net.liftweb.json.DefaultFormats ++
       List(new TransformationUtil.MyDateTimeSerializer("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"),
         new TransformationUtil.MyJodaCurrencyUnitSerializer,
@@ -101,5 +105,4 @@ class ExternalPaymentCreatorTestSuite extends FunSuite {
     val trs = PaymentJsonProcessor.extractIncomingPaymentTransactionsFromPaymentOrdersJSON(jsonStr)
     assert(trs.getOrElse(Nil).length == 6)
   }
-
 }
